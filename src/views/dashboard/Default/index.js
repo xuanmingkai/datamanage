e70d99c606988b4d74695548752fab1fb4186596
Mingkai Xuan <mingkai.xuan@gmail.com>
2022-08-15 14:44:32 +0800

    Add EarningCard and TotalOrderChartCard in Dashboard

--- src/views/dashboard/Default/index.js
+++ src/views/dashboard/Default/index.js
@@ -1,13 +1,30 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
+
+import { Grid } from "@mui/material";
+
+import { gridSpacing } from "../../../store/constant";
+import EarningCard from './EarningCard'
+import TotalOrderLineChartCard from './TotalOrderLineChartCard'
 
 const Dashboard = () => {
-	const [isLoading, setLoading] = useState(true)
-	useEffect(() => {
-		setLoading(false)
-	}, [])
-	return (
-		<h3>Hello world isLoading={isLoading}</h3>
-	)
-}
+  const [isLoading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+  return (
+    <Grid container spacing={gridSpacing}>
+      <Grid item xs={12}>
+        <Grid container spacing={gridSpacing}>
+          <Grid item lg={4} md={6} sm={6} xs={12}>
+            <EarningCard isLoading={isLoading} />
+          </Grid>
+          <Grid item lg={4} md={6} sm={6} xs={12}>
+            <TotalOrderLineChartCard isLoading={isLoading} />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
