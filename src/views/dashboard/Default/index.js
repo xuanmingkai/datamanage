c233b3a9f0cdffa4019fcd3d42c372410768fccc
Mingkai Xuan <mingkai.xuan@gmail.com>
2022-09-06 11:26:35 +0800

    Add PopularCard and TotalGrowthBarChart in Dashboard

--- src/views/dashboard/Default/index.js
+++ src/views/dashboard/Default/index.js
@@ -3,7 +3,9 @@ import React, { useEffect, useState } from "react";
 import { Grid } from "@mui/material";
 
 import { gridSpacing, NodeTypes } from "../../../store/constant";
-import NodeTypeChartCard from './NodeTypeChartCard'
+import NodeTypeChartCard from "./NodeTypeChartCard";
+import TotalGrowthBarChart from "./TotalGrowthBarChart";
+import PopularCard from "./PopularCard";
 
 const Dashboard = () => {
   const [isLoading, setLoading] = useState(true);
@@ -15,16 +17,35 @@ const Dashboard = () => {
       <Grid item xs={12}>
         <Grid container spacing={gridSpacing}>
           <Grid item lg={3} md={6} sm={6} xs={12}>
-            <NodeTypeChartCard isLoading={isLoading} NodeType={NodeTypes.PC}/>
+            <NodeTypeChartCard isLoading={isLoading} NodeType={NodeTypes.PC} />
           </Grid>
           <Grid item lg={3} md={6} sm={6} xs={12}>
-            <NodeTypeChartCard isLoading={isLoading} NodeType={NodeTypes.Camera}/>
+            <NodeTypeChartCard
+              isLoading={isLoading}
+              NodeType={NodeTypes.Camera}
+            />
           </Grid>
           <Grid item lg={3} md={6} sm={6} xs={12}>
-            <NodeTypeChartCard isLoading={isLoading} NodeType={NodeTypes.Router}/>
+            <NodeTypeChartCard
+              isLoading={isLoading}
+              NodeType={NodeTypes.Router}
+            />
           </Grid>
           <Grid item lg={3} md={6} sm={6} xs={12}>
-            <NodeTypeChartCard isLoading={isLoading} NodeType={NodeTypes.Server}/>
+            <NodeTypeChartCard
+              isLoading={isLoading}
+              NodeType={NodeTypes.Server}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Grid container spacing={gridSpacing}>
+              <Grid item md={8} xs={12}>
+                <TotalGrowthBarChart isLoading={isLoading} />
+              </Grid>
+              <Grid item md={4} xs={12}>
+                <PopularCard isLoading={isLoading} />
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
