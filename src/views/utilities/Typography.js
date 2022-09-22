f53c78581336735327e015cc56d31705b1919029
Mingkai Xuan <mingkai.xuan@gmail.com>
2022-09-22 11:26:06 +0800

    Add heading and subtitle in Typography

--- src/views/utilities/Typography.js
+++ src/views/utilities/Typography.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
 
-const Typography = () => {
-	return (
-		<h3>HeiHei</h3>
-	)
-}
+import { Grid } from "@mui/material";
+import MuiTypography from '@mui/material/Typography'
 
-export default Typography
+import MainCard from "../../ui-component/cards/MainCard";
+import SubCard from '../../ui-component/cards/SubCard'
+import { gridSpacing } from "../../store/constant";
+
+const Typography = () => (
+  <MainCard title="Basic Typography">
+    <Grid container spacing={gridSpacing}>
+      <Grid item xs={12} sm={6}>
+        <SubCard title="Heading">
+          <Grid container direction="column" spacing={1}>
+            <Grid item>
+              <MuiTypography variant="h1" gutterBottom>
+                h1. Heading
+              </MuiTypography>
+              <MuiTypography variant="h2" gutterBottom>
+                h2. Heading
+              </MuiTypography>
+            </Grid>
+          </Grid>
+        </SubCard>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <SubCard title="Sub title">
+          <Grid container direction="column" spacing={1}>
+            <Grid item>
+              <MuiTypography variant="subtitle1" gutterBottom>
+                subtitle1. 
+              </MuiTypography>
+            </Grid>
+            <Grid item>
+              <MuiTypography variant="subtitle2" gutterBottom>
+                subtitle1. 
+              </MuiTypography>
+            </Grid>
+          </Grid>
+        </SubCard>
+      </Grid>
+    </Grid>
+  </MainCard>
+);
+
+export default Typography;
