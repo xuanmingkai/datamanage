11059d9b673503093deeb8bf972b2cf0d726a8c1
Mingkai Xuan <mingkai.xuan@gmail.com>
2022-09-27 16:36:19 +0800

    Optimize styles with UserManagement

--- src/views/pages/management/UserManagement.js
+++ src/views/pages/management/UserManagement.js
@@ -1,23 +1,28 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 
 import { styled, useTheme } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
-import TableContainer from "@mui/material/TableContainer";
-import Paper from "@mui/material/Paper";
-import Table from "@mui/material/Table";
-import TableHead from "@mui/material/TableHead";
-import TableBody from "@mui/material/TableBody";
-import TableRow from "@mui/material/TableRow";
+import {
+  Box,
+  Button,
+  Container,
+  Paper,
+  IconButton,
+  Stack,
+  TableContainer,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableFooter,
+  TablePagination,
+  Typography,
+} from "@mui/material";
+
 import TableCell, { tableCellClasses } from "@mui/material/TableCell";
-import TableFooter from "@mui/material/TableFooter";
-import TablePagination from "@mui/material/TablePagination";
-import IconButton from "@mui/material/IconButton";
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
 
 import FirstPageIcon from "@mui/icons-material/FirstPage";
 import LastPageIcon from "@mui/icons-material/LastPage";
@@ -25,14 +30,15 @@ import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
 
 import { userActions } from "../../../services/user/UserActions";
+import MainCard from "../../../ui-component/cards/MainCard";
 
 const TableCellWrapper = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.secondary.color,
-    color: theme.palette.primary.color,
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
   },
   [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
+    fontSize: 12,
   },
 }));
 
@@ -159,121 +165,126 @@ const UserManagement = () => {
   };
 
   return (
-    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-      <Stack justifyContent="center" alignItems="flex-end">
-        <Button variant="contained" disableElevation href="/pages/register">
-          Add User
-        </Button>
-      </Stack>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 300 }} aria-label="custom pagination table">
-          <TableHead>
-            <TableRow>
-              <TableCellWrapper align="center">Id</TableCellWrapper>
-              <TableCellWrapper align="left">UserName</TableCellWrapper>
-              <TableCellWrapper align="left">firstName</TableCellWrapper>
-              <TableCellWrapper align="left">lastName</TableCellWrapper>
-              <TableCellWrapper align="left">Email</TableCellWrapper>
-              <TableCellWrapper align="center">Status</TableCellWrapper>
-              <TableCellWrapper align="center">Operation</TableCellWrapper>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows &&
-              (rowsPerPage > 0
-                ? rows.slice(
-                    page * rowsPerPage,
-                    page * rowsPerPage + rowsPerPage
-                  )
-                : rows
-              ).map((row) => (
-                <TableRowWrapper key={row.id}>
-                  <TableCellWrapper style={{ width: 60 }} align="left">
-                    {row.id}
-                  </TableCellWrapper>
-                  <TableCellWrapper style={{ width: 160 }} align="left">
-                    {row.username}
-                  </TableCellWrapper>
-                  <TableCellWrapper style={{ width: 160 }} align="left">
-                    {row.firstName}
-                  </TableCellWrapper>
-                  <TableCellWrapper style={{ width: 160 }} align="left">
-                    {row.lastName}
-                  </TableCellWrapper>
-                  <TableCellWrapper style={{ width: 160 }} align="left">
-                    {row.email}
-                  </TableCellWrapper>
-                  <TableCellWrapper
-                    style={{ width: 160 }}
-                    align="left"
-                  ></TableCellWrapper>
-                  <TableCellWrapper
-                    style={{ width: 160 }}
-                    align="left"
-                    component="th"
-                    scope="row"
-                  >
-                    <Stack direction="row" spacing={1}>
-                      <Button
-                        variant="contained"
-                        disableElevation
-                        onClick={() => {
-                          handleCheckUserInfo(row.id);
-                        }}
-                      >
-                        Info
-                      </Button>
-                      <Button
-                        variant="contained"
-                        disableElevation
-                        onClick={() => {
-                          handleEditUser(row.id);
-                        }}
-                      >
-                        Edit
-                      </Button>
-                      <Button
-                        variant="contained"
-                        disableElevation
-                        onClick={() => {
-                          handleDeleteUser(row.id);
-                        }}
-                      >
-                        Delete
-                      </Button>
-                    </Stack>
-                  </TableCellWrapper>
-                </TableRowWrapper>
-              ))}
-            {emptyRows > 0 && (
-              <TableRow style={{ height: 53 * emptyRows }}>
-                <TableCell colSpan={6} />
+    <MainCard title="User Management">
+      <Container maxWidth="lg">
+        <Box sx={{ pb: 1.5 }}>
+          <Stack justifyContent="space-between" alignItems="flex-start">
+            <Button variant="contained" disableElevation href="/pages/register">
+              Add User
+            </Button>
+          </Stack>
+        </Box>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 300 }} aria-label="custom pagination table">
+            <TableHead>
+              <TableRow>
+                <TableCellWrapper align="center">Id</TableCellWrapper>
+                <TableCellWrapper align="left">UserName</TableCellWrapper>
+                <TableCellWrapper align="left">firstName</TableCellWrapper>
+                <TableCellWrapper align="left">lastName</TableCellWrapper>
+                <TableCellWrapper align="left">Email</TableCellWrapper>
+                <TableCellWrapper align="center">Status</TableCellWrapper>
+                <TableCellWrapper align="center">Operation</TableCellWrapper>
               </TableRow>
-            )}
-          </TableBody>
-          <TableFooter>
-            <TableRow>
-              <TablePagination
-                rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
-                colSpan={7}
-                count={rows.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                SelectProps={{
-                  inputProps: {
-                    "aria-label": "rows per page",
-                  },
-                  native: true,
-                }}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-                ActionsComponent={TablePaginationActions}
-              />
-            </TableRow>
-          </TableFooter>
-        </Table>
-      </TableContainer>
-    </Container>
+            </TableHead>
+            <TableBody>
+              {rows &&
+                (rowsPerPage > 0
+                  ? rows.slice(
+                      page * rowsPerPage,
+                      page * rowsPerPage + rowsPerPage
+                    )
+                  : rows
+                ).map((row) => (
+                  <TableRowWrapper key={row.id}>
+                    <TableCellWrapper style={{ width: 60 }} align="left">
+                      {row.id}
+                    </TableCellWrapper>
+                    <TableCellWrapper style={{ width: 160 }} align="left">
+                      {row.username}
+                    </TableCellWrapper>
+                    <TableCellWrapper style={{ width: 160 }} align="left">
+                      {row.firstName}
+                    </TableCellWrapper>
+                    <TableCellWrapper style={{ width: 160 }} align="left">
+                      {row.lastName}
+                    </TableCellWrapper>
+                    <TableCellWrapper style={{ width: 160 }} align="left">
+                      {row.email}
+                    </TableCellWrapper>
+                    <TableCellWrapper
+                      style={{ width: 160 }}
+                      align="left"
+                    ></TableCellWrapper>
+                    <TableCellWrapper
+                      style={{ width: 200 }}
+                      align="left"
+                      component="th"
+                      scope="row"
+                      padding="none"
+                    >
+                      <Stack direction="row" spacing={1.5} sx={{ pr: 1 }}>
+                        <Button
+                          variant="contained"
+                          disableElevation
+                          onClick={() => {
+                            handleCheckUserInfo(row.id);
+                          }}
+                        >
+                          Info
+                        </Button>
+                        <Button
+                          variant="contained"
+                          disableElevation
+                          onClick={() => {
+                            handleEditUser(row.id);
+                          }}
+                        >
+                          Edit
+                        </Button>
+                        <Button
+                          variant="contained"
+                          disableElevation
+                          onClick={() => {
+                            handleDeleteUser(row.id);
+                          }}
+                        >
+                          Delete
+                        </Button>
+                      </Stack>
+                    </TableCellWrapper>
+                  </TableRowWrapper>
+                ))}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: 53 * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TablePagination
+                  rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+                  colSpan={7}
+                  count={rows.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  SelectProps={{
+                    inputProps: {
+                      "aria-label": "rows per page",
+                    },
+                    native: true,
+                  }}
+                  onPageChange={handleChangePage}
+                  onRowsPerPageChange={handleChangeRowsPerPage}
+                  ActionsComponent={TablePaginationActions}
+                />
+              </TableRow>
+            </TableFooter>
+          </Table>
+        </TableContainer>
+      </Container>
+    </MainCard>
   );
 };
 
