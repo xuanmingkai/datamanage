442b8b68b38bd07de7706d1b451ecc49ac93b3b9
Mingkai Xuan <mingkai.xuan@gmail.com>
2022-09-27 15:22:26 +0800

    Add MainCard in ImportFile

--- src/views/pages/interface/ImportFile.js
+++ src/views/pages/interface/ImportFile.js
@@ -1,16 +1,15 @@
 import * as React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import CssBaseline from "@mui/material/CssBaseline";
-import Paper from "@mui/material/Paper";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
+import { Box, Button, Paper, Typography } from "@mui/material";
+
 import { ImportFilesActions } from "../../../services/import/ImportFilesActions";
+import MainCard from "../../../ui-component/cards/MainCard";
 
 const ImportFile = () => {
   const im_processing = useSelector((state) => state.importfiles.processing);
   const im_result = useSelector((state) => state.importfiles.result);
-  const im_duration = useSelector((state) => state.importfiles.duration)
-  const im_content = useSelector((state) => state.importfiles.content)
+  const im_duration = useSelector((state) => state.importfiles.duration);
+  const im_content = useSelector((state) => state.importfiles.content);
 
   const dispatch = useDispatch();
 
@@ -44,53 +43,54 @@ const ImportFile = () => {
 
   React.useEffect(() => {
     if (im_result === true) {
-      console.log("duration: " + im_duration + "ms")
+      console.log("duration: " + im_duration + "ms");
       im_content.forEach((item) => {
-        item.content.then(result => {
-          console.log(item.filename)
-          console.log(result)
-        })
-      })
-      
+        item.content.then((result) => {
+          console.log(item.filename);
+          console.log(result);
+        });
+      });
     }
   }, [im_result, im_duration, im_content]);
 
   return (
     <React.Fragment>
-      <CssBaseline />
-      <Typography align="center">Upload Files</Typography>
-      <Paper
-        sx={{
-          mx: "auto",
-          p: 2,
-          display: "flex",
-          flexDirection: "column",
-          minWidth: 240,
-          minHeight: 240,
-          width: 550,
-          alignItems: "center",
-          backgroundColor: "#edf2f7",
-          border: 1,
-          justifyContent: "center",
-        }}
-      >
-        <label htmlFor="contained-button-file">
-          <input
-            onChange={uploadHandler}
-            style={{ display: "none" }}
-            id="contained-button-file"
-            multiple
-            type="file"
-          />
-          <Button variant="contained" component="span">
-            Upload
-          </Button>
-        </label>
-        <Typography>Support Files.</Typography>
-        <Typography>ZIP, CSV, XML.</Typography>
-      </Paper>
+      <MainCard title="Import File">
+        <Paper
+          sx={{
+            mx: "auto",
+            p: 2,
+            display: "flex",
+            flexDirection: "column",
+            minWidth: 240,
+            minHeight: 240,
+            maxWidth: 550,
+            alignItems: "center",
+            backgroundColor: "#edf2f7",
+            border: 1,
+            justifyContent: "center",
+          }}
+        >
+          <Box sx={{ p: 2.5 }}>
+            <label htmlFor="contained-button-file">
+              <input
+                onChange={uploadHandler}
+                style={{ display: "none" }}
+                id="contained-button-file"
+                multiple
+                type="file"
+              />
+              <Button variant="contained" component="span">
+                Upload
+              </Button>
+            </label>
+          </Box>
+          <Typography>Support Files.</Typography>
+          <Typography>ZIP, CSV, XML.</Typography>
+        </Paper>
+      </MainCard>
     </React.Fragment>
   );
 };
 
-export default ImportFile
+export default ImportFile;
