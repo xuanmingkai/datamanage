30a185440d510f6c87c586f8331598e7c6004df2
Mingkai Xuan <mingkai.xuan@gmail.com>
2022-09-27 17:00:46 +0800

    Add TablerIcons Module

--- src/views/utilities/TablerIcons.js
+++ src/views/utilities/TablerIcons.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { styled } from '@mui/material/styles';
+import { Card } from '@mui/material';
 
-const TablerIcons = () => {
-	return (
-		<h3>TablerIcons</h3>
-	)
-}
+// project imports
+import MainCard from '../../ui-component/cards/MainCard';
 
-export default TablerIcons
+// styles
+const IFrameWrapper = styled('iframe')(({ theme }) => ({
+    height: 'calc(100vh - 210px)',
+    border: '1px solid',
+    borderColor: theme.palette.primary.light
+}));
+
+// =============================|| TABLER ICONS ||============================= //
+
+const TablerIcons = () => (
+    <MainCard title="Tabler Icons">
+        <Card sx={{ overflow: 'hidden' }}>
+            <IFrameWrapper title="Tabler Icons" width="100%" src="https://tablericons.com/" />
+        </Card>
+    </MainCard>
+);
+
+export default TablerIcons;
